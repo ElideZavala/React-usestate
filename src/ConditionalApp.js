95160dd491f4525640bf32d7f4c43bfc8457b127
ElideZavala <zavalavinagreelide@gmail.com>
2022-06-09 13:15:44 -0500

    ConditionalApp

--- src/ConditionalApp.js
+++ src/ConditionalApp.js
@@ -10,13 +10,12 @@ const ConditionalApp = () => {
 			 Toggle 
 		 </button>
 
-		 {
-			 condition 
-			 ? <h1>Show message in true</h1>
-			 : <h1>Another message in false</h1>
-		 }
+		 {/* {
+			 condition && 
+			 <h1>Show message in true</h1>
+		 } */}
 
-		 <h1>ConditionalApp</h1>
+		 <h1>State value is { condition.toString() } </h1>
 	 </div>
   )
 }
